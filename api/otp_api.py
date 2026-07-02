40331f2165eecef3e99bf70d1e3e4d06cf6c1fd5
Tony <chidubemigwe449@gmail.com>
2026-07-02 15:14:31 +0100

    refactor: separate OTP layer into api/otp_api.py - Decouples OTP routing from main application file - Cleans up router registration in main.py

---
+++ api/otp_api.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends
+from dependency.db import get_db
+from sqlalchemy.orm import Session
+from schema.user_schema import RequestOTP, UserCreate, VerifyOTP
+from services.messaging_service import send_message
+from services.otp_service import create_otp, verify_otp
+from services.user_service import create_user
+from utils import normalize_phone as np
+
+router = APIRouter(prefix='/otp')
+
+@router.post('/send')
+def get_otp(
+    data: RequestOTP,
+    db: Session = Depends(get_db)):
+    
+    normalized_phone = np.normalize_phone_numbers(data.phone_number)
+    
+    otp = create_otp(db=db, data=data)
+    
+    if not otp.get('success'):
+        return {
+            'success': otp.get('success'),
+        }
+    message = f'{otp.get('message')}, this is your OTP: {otp.get('otp')}'
+    send_message(message=message, phone=normalized_phone)
+    return otp
+
+@router.post('/verify')
+async def handle_otp(
+    data: VerifyOTP,
+    db: Session = Depends(get_db)):
+    
+    result = verify_otp(db=db, data=data)
+    
+    if not result.get('valid'):
+        return {
+            'message': result.get('message')
+        }
+    
+    # create new user
+    normalized_phone = np.normalize_phone_numbers(data.phone_number)
+    payload = UserCreate(phone_number=normalized_phone)
+    
+    user = await create_user(db=db, user=payload)
+    print(user)
+    
+    if not user.get('success'):
+        send_message(message='Unable to create user account please try again!', phone=normalized_phone)
+    
+    # send message to user for onboarding
+    send_message(message='Welcome i\'m Reece your smart ai assistant, whats your name', phone=normalized_phone)
+    return result
+    
+    
